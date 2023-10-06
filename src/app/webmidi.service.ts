13ced38118edf9fe06a4ea0df8e583b89016b707
Michael Haynie <michael.dean.haynie@gmail.com>
2023-10-06 09:00:39 -0700

    add more midi debugging

--- src/app/webmidi.service.ts
+++ src/app/webmidi.service.ts
@@ -27,16 +27,20 @@ export class WebmidiService {
 
     window.navigator.permissions.query({name: "midi"} as unknown as PermissionDescriptor).then((result) => {
       if (result.state === "granted") {
+        document.body.innerHTML += `<div>access granted</div>`
         // Access granted.
       } else if (result.state === "prompt") {
         // Using API will prompt for permission
+        document.body.innerHTML += `<div>api will prompt</div>`
       }
       // Permission was denied by user prompt or permission policy
+      document.body.innerHTML += `<div>permission was denied</div>`
     });
 
 
     if (!!window.navigator.requestMIDIAccess) {
       document.body.innerHTML += `<div>requestMIDIAccess is a thing</div>`
+      document.body.innerHTML += `<div>typeof requestMIDIAccess: ${typeof window.navigator.requestMIDIAccess}</div>`
     } else {
       document.body.innerHTML += `<div>requestMIDIAccess is not a thing</div>`
     }
