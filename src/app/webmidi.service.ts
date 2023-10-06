2c15fa96464d691c1b2fca743fdca796c3f2e41f
Michael Haynie <michael.dean.haynie@gmail.com>
2023-10-06 13:24:22 -0700

    finish ts conversion of webmidi service

--- src/app/webmidi.service.ts
+++ src/app/webmidi.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Message, Note, NoteMessageEvent, WebMidi} from "webmidi";
+import {Message} from "webmidi";
 import {Subject} from "rxjs";
 
 @Injectable({
@@ -8,18 +8,18 @@ import {Subject} from "rxjs";
 export class WebmidiService {
   readonly messageSubject: Subject<Message> = new Subject<Message>();
   private _midiAccess?: MIDIAccess;
-  private _midiInputs?: MIDIInputMap;
-  private _midiOutputs?: MIDIOutputMap;
+  // private _midiInputs?: MIDIInputMap;
+  // private _midiOutputs?: MIDIOutputMap;
 
 
   constructor() {
     if (window.navigator.requestMIDIAccess) {
       window.navigator.requestMIDIAccess().then(
-        (midiAccess) => this.success(midiAccess)
-        // () => { window.document.body.innerHTML += "requestMIDIAccess() failed." }
+        (midiAccess) => this.accessGrantedHandler(midiAccess),
+        console.log // on reject
       );
     } else {
-      // // window.document.body.innerHTML += "Web MIDI API is not available on your browser.\n";
+      console.log('Web MIDI API is not available on your browser.')
     }
   }
 
@@ -30,113 +30,54 @@ export class WebmidiService {
     return this._midiAccess;
   }
 
-  private get midiInputs(): MIDIInputMap {
-    if (!this._midiInputs) {
-      throw new Error();
-    }
-    return this._midiInputs;
-  }
+  // private get midiInputs(): MIDIInputMap {
+  //   if (!this._midiInputs) {
+  //     throw new Error();
+  //   }
+  //   return this._midiInputs;
+  // }
+  //
+  // private get midiOutputs(): MIDIOutputMap {
+  //   if (!this._midiOutputs) {
+  //     throw new Error();
+  //   }
+  //   return this._midiOutputs;
+  // }
 
-  private get midiOutputs(): MIDIOutputMap {
-    if (!this._midiOutputs) {
-      throw new Error();
-    }
-    return this._midiOutputs;
-  }
-
-  private success(midiAccess: MIDIAccess): void {
+  private accessGrantedHandler(midiAccess: MIDIAccess): void {
     this._midiAccess = midiAccess;
 
     this.midiAccess.addEventListener('statechange', (event) => {
       const midiConnectionEvent = event as MIDIConnectionEvent;
-      const str = "port: " + midiConnectionEvent.port.name + ", connected.";
-      // window.document.body.innerHTML += str + "\n";
-
+      // console.log(`Port: ${midiConnectionEvent.port.name}, statechange`, midiConnectionEvent);
       this.setupEventHandler();
     });
-    // this._midiAccess.onconnect = (event) => {
-    //   const str = "port: " + event.port.name + ", connected.";
-    //   // window.document.body.innerHTML += str + "\n";
-    //
-    //   this.setupEventHandler();
-    // }
 
     this.setupEventHandler();
   }
 
   private setupEventHandler(): void {
-    // if (typeof this.midiAccess.inputs === "function") {
-    //   inputs = this.midiAccess.inputs();
-    // } else {
-      const inputs: MIDIInput[] = [];
-      this.midiAccess.inputs.forEach(input => {
-        inputs.push(input);
-      });
-      // const iter = this.midiAccess.inputs.values();
-      // inputs = [];
-      // for (let o = iter.next(); !o.done; o = iter.next()) {
-      //   inputs.push(o.value);
-      // }
-    // }
+    const inputs: MIDIInput[] = [];
+    this.midiAccess.inputs.forEach(input => {
+      inputs.push(input);
+    });
 
     for (let port = 0; port < inputs.length; port++) {
       const str = "input port:" + port + ", name: " + inputs[port].name;
-      // window.document.body.innerHTML += str + "\n";
 
       // leaving IIFE as-is for now
       (function (messageSubject) {
-        var _port = port;
+        const _port = port; // copy (do not share ref)
         inputs[_port].onmidimessage = function (event) {
           const midiMessageEvent = event as MIDIMessageEvent;
-          var str = "port:" + _port + " "; // + ", receivedTime:" + event.receivedTime.toFixed(2) + ", ";
-          for (var i = 0; i < midiMessageEvent.data.length; i++) {
-            str += midiMessageEvent.data[i].toString(16) + " ";
-          }
-          console.log('midiMessage: ', event);
           const message = new Message(midiMessageEvent.data);
-          console.log('Message: ', message);
           messageSubject.next(message);
 
-          // window.document.body.innerHTML += str + "\n";
         };
 
-        // inputs[_port].ondisconnect = function (event) {
-        //   var str = "input port:" + _port + ", disconnected.";
-        //   // window.document.body.innerHTML += str + "\n";
-        // };
-      }(this.messageSubject));
+      }(this.messageSubject)); // inject subject into IIFE
     }
-
-    // if (typeof this.midiAccess.outputs === "function") {
-    //   outputs = this.midiAccess.outputs();
-    // } else {
-      const outputs: MIDIOutput[] = [];
-      this.midiAccess.outputs.forEach(output => {
-        outputs.push(output)
-      });
-      // const iter = this.midiAccess.outputs.values();
-      // outputs = [];
-      // for (let o = iter.next(); !o.done; o = iter.next()) {
-      //   outputs.push(o.value);
-      // }
-    // }
-
-    for (let port = 0; port < outputs.length; port++) {
-      // leaving IIFE as-is for now
-      (function () {
-        var _port = port;
-        // outputs[_port].ondisconnect = function (event) {
-        //   var str = "output port:" + _port + ", disconnected.";
-        //   // window.document.body.innerHTML += str + "\n";
-        // };
-      }());
-    }
-
-    // never used anyway?
-    // this._midiInputs = inputs;
-    // this._midiOutputs = outputs;
 
   }
-
 
 }
