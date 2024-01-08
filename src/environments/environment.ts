d01fb77ab6b4712f4f641b8fa1d557a8e4069244
Michael Haynie <michael.dean.haynie@gmail.com>
2024-01-07 17:21:26 -0700

    fix type in environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   name: 'production',
   websocketUrl: 'wss://inalltwelvekeys.com:443',
-  apiUrl: 'https://inalltwelvekwys.com:443/api'
+  apiUrl: 'https://inalltwelvekeys.com:443/api'
 };
